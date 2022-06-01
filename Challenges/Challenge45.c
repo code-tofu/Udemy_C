75713dbf9143242d0880c57d6251b8f6aee8a385
code-tofu <89731295+code-tofu@users.noreply.github.com>
2022-06-01 14:56:13 +0800

    Update Challenge45.c

--- Challenges/Challenge45.c
+++ Challenges/Challenge45.c
@@ -6,19 +6,18 @@ You can use the %zd format specifier or %u (unsigned)
 Use sizeof operator
   
 */
-  #include <stdio.h>
+#include <stdio.h>
 
-int main() {
-  printf("Size of int: %zd bytes \n",sizeof(int));
-  printf("Size of char: %zd bytes\n",sizeof(char));
-  printf("Size of short: %zd bytes\n",sizeof(short));
-  printf("Size of long: %zd bytes\n",sizeof(long));
-  // note that sizeof takes in a variable with a space i.e. it is a operator not a function
-  printf("Size of long long: %zd bytes\n",sizeof(long long));
-  printf("Size of double: %zd bytes\n",sizeof(double));
-  printf("Size of float: %zd bytes\n",sizeof(float));
-  printf("Size of long double: %zd bytes\n",sizeof(long double));
-  
-  
-    return 0; 
+int main(){
+	printf("Size of int: %zd bytes \n",sizeof(int));
+	printf("Size of char: %zd bytes\n",sizeof(char));
+	printf("Size of short: %zd bytes\n",sizeof(short));
+	printf("Size of long: %zd bytes\n",sizeof(long));
+	// note that sizeof takes in a variable with a space i.e. it is a operator not a function
+	printf("Size of long long: %zd bytes\n",sizeof(long long));
+	printf("Size of double: %zd bytes\n",sizeof(double));
+	printf("Size of float: %zd bytes\n",sizeof(float));
+	printf("Size of long double: %zd bytes\n",sizeof(long double));
+
+	return 0;
 }
