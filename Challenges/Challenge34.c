46786ea584ecc097375c3c0cb0ac3da54b6f74fb
code-tofu <89731295+code-tofu@users.noreply.github.com>
2022-06-01 14:44:14 +0800

    Update Challenge34.c

--- Challenges/Challenge34.c
+++ Challenges/Challenge34.c
@@ -6,29 +6,27 @@ Print with line feeds
   */
 #include <stdio.h>
 
-int main() {
+int main(){
 
-// Declare and initialise variables
-double width=0.0, height =0.0, perimeter =0.0, area =0.0;
+	// Declare and initialise variables
+	double width=0.0, height =0.0, perimeter =0.0, area =0.0;
 
-//Read variables
-printf("Input Width:");
-scanf("%lf", &width);
-printf("Input Height:");
-scanf("%lf", &height);  
-//Remember that you are scanf to a variable address
+	//Read variables
+	printf("Input Width:");
+	scanf("%lf", &width);
+	printf("Input Height:");
+	scanf("%lf", &height);  
+	//Remember that you are scanf to a variable address
 
-//Computation
-perimeter = 2 *(width + height);
-area = width * height;
+	//Computation
+	perimeter = 2 *(width + height);
+	area = width * height;
 
-  
-//Print Perimeter and Area
-printf("For a rectangle of width: %lf and height: %lf the perimeter is %lf and the area is %lf", width,height,perimeter,area);
+		//Print Perimeter and Area
+	printf("For a rectangle of width: %lf and height: %lf the perimeter is %lf and the area is %lf", width,height,perimeter,area);
 
-//Print Perimeter and Area to 2dec
-printf("For a rectangle of width: %.2lf and height: %.2lf the perimeter is %.2lf and the area is %.2lf", width,height,perimeter,area);
+	//Print Perimeter and Area to 2dec
+	printf("For a rectangle of width: %.2lf and height: %.2lf the perimeter is %.2lf and the area is %.2lf", width,height,perimeter,area);
 
-  return 0;
+	  return 0;
 }
-  
