63f63e3a8d1fc643875503f43175631a783e18c4
Benjamin Foo <benjamin.ftc@gmail.com>
2022-06-30 00:26:30 +0800

    challenge 70

---
+++ Challenges/Challenge70.c
@@ -0,0 +1,42 @@
+
+
+/*
+Player One Uses "X"
+Player 2 Uses "O"
+
+Can either do in 1D Array or 2D Array
+
+Printing:
+1D will be 3 or 6 + i
+2D will be j then i
+
+Winning:
+Check across rows i1 = i2 =i3 looped over j OR
+Check down columnns j1 = j2 = j3 looped over i OR
+Check Diagonals ij1 = ij2 = ij3 looped over ij OR
+
+Modify:
+Modify Array Position Directly OR Modify Row-Column based on number using modulo
+
+Programme 
+Check for Win
+    Print Win, Terminate
+Print Board
+Modify Board
+
+*/
+
+//initialise tic tac toe array. 1D size 10 or 2D 3x3 i.e size 10
+
+
+
+// Printing Function
+
+
+
+
+// Check for Win
+
+
+
+// Modify  size
