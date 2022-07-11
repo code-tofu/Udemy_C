0668f68e8b7b20a64cf101f4d9d03f038e8229eb
code_tofu <benjamin.ftc@gmail.com>
2022-07-11 18:18:20 +0800

    Updated C70 c72

---
+++ Challenges/Challenge72.c
@@ -0,0 +1,13 @@
+/* NOTES:
+
+Create a tic tac toe game, player 1 vs 2
+3 by 3 grid
+type char array
+
+create the functions
+- check for win
+- draw board
+- markboard and check for invalid marking
+
+
+*/
